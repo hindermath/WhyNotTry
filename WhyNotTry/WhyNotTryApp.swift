f06f00215751bbbde34e951efc0a75cc636ae558
Thorsten Hindermann <hindermath@googlemail.com>
2024-02-21 12:16:15 +0100

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Thorsten Hindermann on 21.02.24.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
