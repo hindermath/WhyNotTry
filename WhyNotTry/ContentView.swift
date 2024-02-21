e2448a9c2c70d7c9e914ca1c7e23ef0051d51d84
Thorsten Hindermann <hindermath@googlemail.com>
2024-02-21 12:47:48 +0100

    Add .gitignore file...
    ...with Xcode contreent from gitignore.io

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Circle()
+                .fill(.blue)
+                .padding()
+                .overlay(
+                    Image(systemName: "figure.archery")
+                        .font(.system(size:144))
+                        .foregroundColor(.white)
+                )
+            
+            Text("Archery")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
         }
-        .padding()
     }
 }
 
