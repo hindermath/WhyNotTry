132c6eec1f111466508a27089d7cef61d4595663
Thorsten Hindermann <hindermath@googlemail.com>
2024-02-22 12:57:27 +0100

    Transitions effect added

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
     var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
     var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
     @State private var selected = "Archery"
+    @State private var id = 1
     var body: some View {
         VStack {
             Text("Why not try...")
@@ -31,13 +32,15 @@ struct ContentView: View {
                 
                 Text("\(selected)")
                     .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-            }
+            }.transition(.slide)
+                .id(id)
             
             Spacer()
             
             Button("Try again...") {
                 withAnimation(.easeInOut(duration: 1)) {
                     selected = activities.randomElement() ?? "Archery"
+                    id += 1
                 }
                 
             }.buttonStyle(.borderedProminent)
